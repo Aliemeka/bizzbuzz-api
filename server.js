81ab02885cbe71e1cba2c82c8ec42f118eed6a13
Emeka Allison <emekaallison4@gmail.com>
2021-04-29 15:58:33 +0100

    Dep: Added morgan for logging

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const routes = require('./routes')
+const morgan = require('morgan')
+
+const routes = require('./routes');
 
 const app = express();
- app.use(express.json());
+
+// Middlewares
+app.use(morgan('dev'))
+app.use(express.json());
 
  // Routes
 routes(app);
