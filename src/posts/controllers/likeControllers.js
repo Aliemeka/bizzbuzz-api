c2bd687bcf4ee5df1de462baaa8e3b630400d2c1
Emeka Allison <emekaallison4@gmail.com>
2021-05-03 01:50:31 +0100

    Feat: Get all post likes

--- src/posts/controllers/likeControllers.js
+++ src/posts/controllers/likeControllers.js
@@ -1,8 +1,8 @@
 const { getPost } = require('../services/postServices');
-const { createOrDeleteLike } = require('../services/likeServices');
+const { createOrDeleteLike, findLikes } = require('../services/likeServices');
 const { handleValidationErrors } = require('../../../utils/errorHandlers');
 
-// Returns all post
+// Like or unlike a post
 module.exports.addOrRemoveLike = async (req, res) =>{
     const { postId } = req.params;
     const { id } = req.user;
@@ -14,6 +14,32 @@ module.exports.addOrRemoveLike = async (req, res) =>{
         }
         const message = await createOrDeleteLike(postId, id);
         res.status(200).json({ success: true, message });
+    }
+    catch(error){
+        const errors = handleValidationErrors(err);
+        if(Object.entries(errors).length){
+            res.status(400).json({ errors });
+        }
+        else{
+            res.status(500).json({ error: "Error editting post"});
+        }
+    }
+}
+
+// Get the likes of a post
+module.exports.addOrRemoveLike = async (req, res) =>{
+    const { postId } = req.params;
+
+    try{
+        const post = await getPost(postId);
+        if(!post){
+            return res.status(404).json({ success: false, error: "Post not found" })
+        }
+        const likes = await findLikes(postId);
+        if(!likes){
+            return res.status(404).json({ success: false, error: "Post not found" })
+        }
+        res.status(200).json({ success: true, likes });
     }
     catch(error){
         const errors = handleValidationErrors(err);
--- src/posts/services/likeServices.js
+++ src/posts/services/likeServices.js
@@ -3,21 +3,30 @@ const Post = require("../models/postModel")
 
 module.exports.createOrDeleteLike = async(postId, userId) =>{
     try{
-        const like = await Like.findOne({ post: postId, user: userId })
+        const like = await Like.findOne({ post: postId, user: userId });
         if(like){
             like.delete();
-            await Post.findByIdAndUpdate(postId, {$pull: { likes: like.id }})
+            await Post.findByIdAndUpdate(postId, {$pull: { likes: like.id }});
             return "unliked"
         }
         else{
             const newLike = await Like.create({ post: postId, user: userId });
             await Post.findByIdAndUpdate(
                 postId, {$push: { likes: newLike.id }}, { new: true, useFindAndModify: false }
-            )
-            return "liked"
+            );
+            return "liked";
         }
     }
     catch(error){
         throw error
+    }
+}
+
+module.exports.findLikes = async(postId) =>{
+    try{
+        return await Like.find({ post: postId });
+    }
+    catch(error){
+        throw error;
     }
 }
