ced0ff814ded278874775064c3ea3b7b5f656163
Emeka Allison <emekaallison4@gmail.com>
2021-07-12 20:28:18 +0100

    Update: Formatted server.js

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
-const start = require('./kernel');
+const start = require("./kernel");
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 const app = express();
 
 // Middlewares
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
- // Routes
+// Routes
 routes(app);
 
-start(app)
-
+start(app);
