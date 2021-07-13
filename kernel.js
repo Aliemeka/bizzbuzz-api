ad6686aa0ed886bb5071dc31087ab946362d03be
Emeka Allison <emekaallison4@gmail.com>
2021-07-13 01:51:33 +0100

    Update: Formatted kernel.js and routes.js

--- kernel.js
+++ kernel.js
@@ -1,15 +1,19 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const { dbUri, PORT } = require("./utils/config")
+const { dbUri, PORT } = require("./utils/config");
 
-module.exports = app =>{
-    
-    mongoose.connect(dbUri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-      })
-    .then(res => app.listen(PORT, ()=>console.log("Web application started at port: " + PORT)))
-    .catch(err => console.log(err))
-}
+module.exports = (app) => {
+  mongoose
+    .connect(dbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    })
+    .then((res) =>
+      app.listen(PORT, () =>
+        console.log("Web application started at port: " + PORT)
+      )
+    )
+    .catch((err) => console.log(err));
+};
--- routes.js
+++ routes.js
@@ -1,21 +1,21 @@
-const postRoutes = require('./src/posts/routes/postRoutes')
-const authRoutes = require('./src/users/routes/authRoutes')
+const postRoutes = require("./src/posts/routes/postRoutes");
+const authRoutes = require("./src/users/routes/authRoutes");
 
-module.exports = app =>{
-    // module routes
-    app.use('/posts', postRoutes);
-    app.use('/auth', authRoutes);
+module.exports = (app) => {
+  // module routes
+  app.use("/posts", postRoutes);
+  app.use("/auth", authRoutes);
 
-    //Default routes
-    // Welcome message
-    app.get('/', (req, res)=>{
-        res.json({
-            message: "Welcome to bizzbuzz"
-        });
+  //Default routes
+  // Welcome message
+  app.get("/", (req, res) => {
+    res.json({
+      message: "Welcome to bizzbuzz",
     });
+  });
 
-    // Default 404 error response
-    app.use((req, res)=>{
-        res.status(404).json({ message: `Invalid endpoint: '${req.url}'` });
-    });
-}
+  // Default 404 error response
+  app.use((req, res) => {
+    res.status(404).json({ message: `Invalid endpoint: '${req.url}'` });
+  });
+};
