09bf152be4db4e9addd1a31b4e07c710e696ae1f
Emeka Allison <emekaallison4@gmail.com>
2021-07-13 01:29:18 +0100

    Update: Formatted post routes

--- src/posts/routes/postRoutes.js
+++ src/posts/routes/postRoutes.js
@@ -7,33 +7,40 @@ const { requireAuth } = require("../../../middlewares/authMiddleware");
 const router = express.Router();
 
 // Get all post
-router.get('/', postControllers.allPost);
+router.get("/", postControllers.allPost);
 // Add all post
-router.post('/', requireAuth, postControllers.addPost);
+router.post("/", requireAuth, postControllers.addPost);
 // Get a post by id
-router.get('/:postId', requireAuth, postControllers.postDetail);
+router.get("/:postId", requireAuth, postControllers.postDetail);
 //Edit a post
-router.put('/:postId', requireAuth, postControllers.editPost);
+router.put("/:postId", requireAuth, postControllers.editPost);
 // Delete a post
-router.delete('/:postId', requireAuth, postControllers.removePost);
-
+router.delete("/:postId", requireAuth, postControllers.removePost);
 
 //Likes
 // Get post likes
-router.get('/:postId/likes', requireAuth, likeControllers.getPostLikes);
+router.get("/:postId/likes", requireAuth, likeControllers.getPostLikes);
 // add or remove like
-router.get('/:postId/like', requireAuth, likeControllers.addOrRemoveLike);
+router.get("/:postId/like", requireAuth, likeControllers.addOrRemoveLike);
 
 //Replies
 // Get post reply
-router.get('/:postId/replies', requireAuth, replyControllers.getReplies);
+router.get("/:postId/replies", requireAuth, replyControllers.getReplies);
 // Reply a post
-router.post('/:postId/replies', requireAuth, replyControllers.replyPost);
+router.post("/:postId/replies", requireAuth, replyControllers.replyPost);
 // Get a reply
-router.get('/:postId/replies/:replyId', requireAuth, replyControllers.getReply);
+router.get("/:postId/replies/:replyId", requireAuth, replyControllers.getReply);
 // Edit a reply
-router.put('/:postId/replies/:replyId', requireAuth, replyControllers.editReply);
+router.put(
+  "/:postId/replies/:replyId",
+  requireAuth,
+  replyControllers.editReply
+);
 // delete reply
-router.delete('/:postId/replies/:replyId', requireAuth, replyControllers.removeReply);
+router.delete(
+  "/:postId/replies/:replyId",
+  requireAuth,
+  replyControllers.removeReply
+);
 
 module.exports = router;
